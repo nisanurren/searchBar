29d5c85105225097251cad8fe162af4d85169204
Nisanur Ençkü <nisanurren@gmail.com>
2024-07-22 17:11:54 +0300

    Add: change promptin variable

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -29,7 +29,7 @@ const SearchModal = ({ open, question, onQuestionChange, onSubmit, chatHistory, 
                     ? entry.content
                     : index < chatHistory.length - 1
                     ? entry.content
-                    : displayedResponse}
+                    : entry.content}
                 </div>
               </div>
             )
