beb7f1cb912fa12a5196572e7ef6e630ee1ecacb
Nisanur Ençkü <nisanurren@gmail.com>
2024-07-23 23:25:57 +0300

    Add: history style

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -10,12 +10,18 @@ function History({ onHistoryItemClick }) {
   }, []);
 
   return (
-    <div>
-      {conversations.map((con) => (
-        <div className="p-2" onClick={() => onHistoryItemClick(con.id)} key={con.id}> {/* Update onClick to use the passed handler */}
-          {con.id}
-        </div>
-      ))}
+    <div style={{ maxWidth: "260px" }} className="p-8 h-screen text-left">
+      <div className="text-gray-600 p-3">Latest Chats:</div>
+      <ul>
+        {conversations.map((item) => (
+          <li
+            onClick={() => onHistoryItemClick(item.id)}
+            className="text-gray-400 p-3 hover:bg-fini-blue hover:text-white rounded-xl hover:rounded-xl`}"
+          >
+            {item.conversation[0].content}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
