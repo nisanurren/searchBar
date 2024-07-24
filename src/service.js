e523d6326bd3dd5c890f74ec9ca13eb3e7062719
Nisanur Ençkü <nisanurren@gmail.com>
2024-07-24 21:20:06 +0300

    Add: change service as class

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axiosInstance from "./axios";
 
-const Service = {
+class Service {
     /**
      * Ask a question to the API
      * @param {string} question - The question to ask
@@ -11,44 +11,12 @@ const Service = {
         return axiosInstance
             .post("/v2/bots/ask-question", { question: question, messageHistory: messageHistory })
             .then((response) => {
-                console.log(0, response);
                 return response;
             })
             .catch((error) => {
-                console.log(error);
                 return error;
             });
-    },
+    }
+}
 
-
-
-    /**
-     * Get chat history
-     * @param {array} source - The source array [“all”,“zendesk”,“slack”,“discord”,“widget”,“intercom”,“ui”,“api”]
-     * @param {number} startEpoch - Start time chat history
-     * @param {number} endEpoch - End time chat history
-     * @returns {Promise<Object>} The response from the API
-     */
-    chatHistory({ source, startEpoch, endEpoch }) {
-        console.log(source)
-        return axiosInstance
-            .get("/v2/bots/requests/public", {
-                params: {
-                    source: source,
-                    startEpoch: startEpoch,
-                    endEpoch: endEpoch
-                }
-            })
-            .then((response) => {
-                return response.data
-            })
-            .catch((error) => {
-                return error;
-            });
-    },
-
-
-
-};
-
-export default Service;
+export default new Service();
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import previousChatsReducer from "./previousChatSlice";
 import userReducer from "./userSlice";
 import questionReducer from "./questionSlice";
-import chatHistoryReducer from "./chatHistorySlice";
 
 export const store = configureStore({
     reducer: {
         previousChats: previousChatsReducer,
         user: userReducer,
         question: questionReducer,
-        chatHistory: chatHistoryReducer,
     },
 });
